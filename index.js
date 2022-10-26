2b73ed4120848a22c545fc3125842c3137485c02
Ambika kumar <95169962+Ambika2510@users.noreply.github.com>
2022-10-27 00:04:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // const dotenv = require('dotenv')
 // dotenv.config();
-const dbURI = 'mongodb+srv://Ambika:Ambika1234@cluster0.m6vw5f9.mongodb.net/node?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://Ambika:@cluster0.m6vw5f9.mongodb.net/node?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => { app.listen('4500') })
